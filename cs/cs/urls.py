496cb0862dda8b3aaf18e6eb07f5b120d686fc41
manankshastri <manank.shastri@gmail.com>
2018-10-29 15:16:43 -0400

    initial phase

--- cs/cs/urls.py
+++ cs/cs/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+
 from django.views.generic import RedirectView
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# Use include() to add paths from the catalog application
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
@@ -31,5 +37,5 @@ urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/')),
 ]
 
-
-
+# use static() to add url mapping to serve static files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
