7ddce67e72e31f2eb561df9c03044ab26d8083f9
manankshastri <manank.shastri@gmail.com>
2018-10-31 17:34:45 -0400

    updated models.py

--- cs/catalog/admin.py
+++ cs/catalog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from .models import Patient, Doctor, Prescription
 # Register your models here.
+
+# admin.site.register(Patient)
+# admin.site.register(Doctor)
+
+
+class PrecInLine(admin.TabularInline):
+    model = Prescription
+
+
+# define the doctor admin class
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'specialty')
+    inlines = [PrecInLine]
+
+
+# register the admin class with the associated model
+admin.site.register(Doctor, DoctorAdmin)
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('ssn', 'first_name', 'last_name')
+
+
+@admin.register(Prescription)
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'doctor', 'desc')
--- cs/catalog/models.py
+++ cs/catalog/models.py
@@ -5,40 +5,41 @@ class Patient(models.Model):
     # model representing patient
     GENDER_CHOICES = [('M', 'Male'), ('F', 'Female')]
     ssn = models.IntegerField('SSN', blank=False, primary_key=True)
-    title = models.CharField(max_length=10, blank=True)
-    suffix = models.CharField(max_length=4, blank=True)
     first_name = models.CharField('First Name', max_length=50)
     last_name = models.CharField('Last Name', max_length=50)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    dob = models.DateTimeField('Date of Birth')
-    address = models.CharField(max_length=20)
-    city = models.CharField(max_length=20)
-    state = models.CharField(max_length=20)
-    zip_code = models.IntegerField()
-    country = models.CharField(max_length=20)
-    phone_number = models.IntegerField()
-    email = models.EmailField()
-    # race = models.CharField(max_length=20)
-    # language = models.CharField(max_length=30)
-    insurance = models.CharField('Health Insurance Carrier', max_length=50, blank=False)
-    credit_card = models.CharField(max_length=30, blank=False)
-    credit_card_number = models.IntegerField(blank=False)
-    description = models.TextField()
-    doctor = models.ManyToManyField('Doctor', blank=True)
 
     class Meta:
         ordering = ['first_name', 'last_name']
 
     def __str__(self):
-        return f'{self.first_name}, {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
 
 
 class Doctor(models.Model):
     # model representing doctor
     PREFIX = [('Dr', 'Dr')]
-    prefix = models.CharField(max_length=1, choices=PREFIX, blank=False)
-    first_name = models.CharField('First Name', max_length=50, blank=False)
-    last_name = models.CharField('Last Name', max_length=50, blank=False)
+    ssn = models.IntegerField('SSN', blank=False, primary_key=True)
+    prefix = models.CharField(max_length=2, choices=PREFIX)
+    first_name = models.CharField('First Name', max_length=50)
+    last_name = models.CharField('Last Name', max_length=50)
+    specialty = models.CharField(max_length=50)
+    patient = models.ManyToManyField(Patient)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 
+class Prescription(models.Model):
+    # model representing prescription
+    patient = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
+    desc = models.TextField('Prescription', help_text='Enter the prescription : ')
+
+    def __str__(self):
+        return f'{self.patient} {self.doctor} {self.desc}'
 
