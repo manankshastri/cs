be9ca67dff00208565552d0653bc62fedf08b19d
manankshastri <manank.shastri@gmail.com>
2018-11-09 17:47:30 -0500

    added templates

--- cs/catalog/views.py
+++ cs/catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Patient, Doctor, Prescription
+
 
 # Create your views here.
+def index(request):
+    """ view function for home page"""
+
+    # generate number of patients
+    num_patients = Patient.objects.all().count()
+
+    # generate number of doctors
+    num_doctors = Doctor.objects.all().count()
+
+    context = {
+        'num_patients': num_patients,
+        'num_doctors': num_doctors,
+    }
+
+    # render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
