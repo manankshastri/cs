61b963cdf1aa48dbe6d692e43bd2562449116a9f
manankshastri <manank.shastri@gmail.com>
2018-11-09 23:45:34 -0500

    modifications

--- cs/catalog/models.py
+++ cs/catalog/models.py
@@ -47,7 +47,7 @@ class Prescription(models.Model):
     # model representing prescription
     patient = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True)
     doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
-    desc = models.TextField('Prescription', help_text='Enter the prescription : ')
+    desc = models.TextField('Prescription', help_text='Enter the prescription : ', null=True)
 
     def __str__(self):
         return f'{self.patient} {self.doctor} {self.desc}'
